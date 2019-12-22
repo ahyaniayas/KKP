d002b97f3f9e6125a7c2368591cdc65d3285a8b2
ahyani.pt-solusi <ahyani@pt-solusi.co.id>
2019-12-22 20:17:18 +0700

    loader

--- arsippdm/application/views/_part/loading.php
+++ arsippdm/application/views/_part/loading.php
@@ -1,4 +1,14 @@
 <div class="frame loading">
 	<span class="helper"></span>
-	<img src="<?= base_url('assets/images/loading.gif') ?>" />
+	<!-- <img src="<?= base_url('assets/images/loading.gif') ?>" /> -->
+    <div class="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 </div>
